f51290a953e2370febcaef05172acf73a184b1b8
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-14 10:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let str = '';
+
+for(let i = 0; i < l; i++){
+   str += Math.floor(Math.random()*100).toString(16).padStart('f',1); 
+}
+	let arr = str.split('');
+	return arr.splice(0,l).join('');
 }
 
 // Do not change the code below.
